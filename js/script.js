b30f94704eefe3ddb8b45b38ff5193b600f62365
Daniel-Ekerhovd <danielekerh@gmail.com>
2023-09-18 20:44:09 +0200

    Finished
    Finished the task and checked it with the answer branch.

---
+++ js/script.js
@@ -0,0 +1,102 @@
+// Question 1
+
+var decimalNumber = 6.9;
+var integerNumber = 32;
+
+console.log("")
+console.log("Question 1");
+console.log(decimalNumber, integerNumber);
+
+
+// Question 2 
+
+var workingDays = "Monday" + "-" + "Tuesday" + "-" + "Wednsday" + "-" + "Thursday" + "-" + "Friday";
+
+console.log("")
+console.log("Question 2");
+console.log(workingDays);
+
+
+//Question 3
+
+var additionVar = 5 + 5;
+var subtractionVar = 32 - 22;
+var multiplicationVar = 2 * 5;
+var divisionVar = 50 / 5;
+
+console.log("")
+console.log("Question 3");
+console.log(additionVar, subtractionVar, multiplicationVar, divisionVar)
+
+
+//Question 4 
+
+var isItNight = false;
+
+
+console.log("")
+console.log("Question 4");
+console.log(isItNight);
+
+
+// Question 5
+
+var frogMood = "The frogs are angry."; 
+
+
+console.log("")
+console.log("Question 5");
+console.log(frogMood.length);
+
+
+//Question 6 
+
+var timeElapsed
+var totalPrice
+
+//These variables are now hunchbacked 
+
+
+//Question 7
+
+var number1 = "53"; 
+var number2 = "44.6";
+
+var number1 = parseInt(number1);
+var number2 = parseFloat(number2);
+
+console.log("")
+console.log("Question 7")
+console.log(typeof number1, typeof number2);
+
+
+//Question 8 
+
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+
+console.log("")
+console.log("Question 8")
+console.log(typeof pet);
+console.log(typeof age);
+console.log(typeof discountApplied);
+console.log(typeof timeLeft);
+
+
+//Question 9
+
+var headingUpdate = "Updating heading";
+var h1 = document.querySelector("h1");
+
+h1.innerHTML = headingUpdate
+
+
+//Question 10
+
+var body = document.querySelector("body");
+
+body.style.backgroundColor = "lightskyblue"
+
